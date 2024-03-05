ffeb29b4382a231d470835ed9fda781de6f44ebb
Billy Wallace <wallace.billy1@gmail.com>
2024-03-05 09:43:14 -0700

    Add test for adding food trucks to the event in event_spec

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,9 +7,14 @@ require './lib/food_truck'
 describe Event do
   before :each do
     @event = Event.new
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
   end
 
   it 'exists' do
     expect(@event).to be_an Event
+  end
+
+  it 'can add food trucks to the event' do
+    expect(@event.food_trucks).to eq []
   end
 end
