e036a9e8188cec6fe3d6f8add7d1d5ba5ba64e79
Billy Wallace <wallace.billy1@gmail.com>
2024-03-05 10:51:26 -0700

    Add sorted_item_list method to Event

--- lib/event.rb
+++ lib/event.rb
@@ -21,4 +21,18 @@ class Event
       food_truck.inventory.include?(item)
     end
   end
+
+  def sorted_item_list
+    all_items = []
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, _|
+        all_items << item.name
+      end
+    end
+    all_items.uniq.sort
+  end
+
+  # sorted_item_list => Array with a list of names (no dupes) of all items that foodtrucks have in stock, alpha order
+  # total_inventory => Hash with Items as keys and sub-hash as values.  Subhash should contain quantity and food_truck keys
+  #overstock_items => Array of Item objects.
 end
