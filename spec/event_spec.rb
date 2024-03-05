661899ae6602ae5f65b241c78475e90ad484ebad
Billy Wallace <wallace.billy1@gmail.com>
2024-03-05 09:49:48 -0700

    Update tests to match interaction pattern

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,19 +6,37 @@ require './lib/food_truck'
 
 describe Event do
   before :each do
-    @event = Event.new
-    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @event = Event.new("South Pearl Street Farmers Market")
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck2 = FoodTruck.new("Palisade Peach Shack")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+    @item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
   end
 
   it 'exists' do
     expect(@event).to be_an Event
   end
 
+  it 'can return the name of the event' do
+    expect(@event.name).to eq "South Pearl Street Farmers Market"
+  end
+
   it 'can add food trucks to the event' do
     expect(@event.food_trucks).to eq []
     
-    @event.add_food_truck(@food_truck)
+    @event.add_food_truck(@food_truck1)
     
-    expect(@event.food_trucks).to eq [@food_truck]
+    expect(@event.food_trucks).to eq [@food_truck1]
+  end
+
+  it 'can return a list of all food truck names at an event' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+  
+    expect(@event.food_truck_names).to eq 
+
   end
 end
