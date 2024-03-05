374dbbe1dd427798779d0eb08d320b59f9e5b0c1
NeilTheSeal <neil.hendren@gmail.com>
2024-03-05 10:07:23 -0700

    Refactor food truck spec

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,35 +3,55 @@ require "./spec/spec_helper"
 # rubocop:disable Metrics/BlockLength
 RSpec.describe FoodTruck do
   before(:each) do
-    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
     @item1 = Item.new({ name: "Peach Pie (Slice)", price: "$3.75" })
     @item2 = Item.new({ name: "Apple Pie (Slice)", price: "$2.50" })
+    @item3 = Item.new({ name: "Peach-Raspberry Nice Cream", price: "$5.30" })
+    @item4 = Item.new({ name: "Banana Nice Cream", price: "$4.25" })
   end
 
   describe "#initialize" do
     it "exists" do
-      expect(@food_truck).to be_an_instance_of(FoodTruck)
+      expect(@food_truck1).to be_an_instance_of(FoodTruck)
     end
 
     it "has attributes" do
-      expect(@food_truck.name).to eq("Rocky Mountain Pies")
-      expect(@food_truck.inventory).to eq({})
+      expect(@food_truck1.name).to eq("Rocky Mountain Pies")
+      expect(@food_truck1.inventory).to eq({})
     end
   end
 
   describe "#stock" do
     it "can check stock of an item" do
-      expect(@food_truck.check_stock(@item1)).to eq(0)
+      expect(@food_truck1.check_stock(@item1)).to eq(0)
     end
 
     it "can stock an item" do
-      @food_truck.stock(@item1, 30)
-      expect(@food_truck.inventory).to eq({ @item1 => 30 })
-      expect(@food_truck.check_stock(@item1)).to eq(30)
-      @food_truck.stock(@item1, 25)
-      expect(@food_truck.check_stock(@item1)).to eq(55)
-      @food_truck.stock(@item2, 12)
-      expect(@food_truck.inventory).to eq({ @item1 => 55, @item2 => 12 })
+      @food_truck1.stock(@item1, 30)
+      expect(@food_truck1.inventory).to eq({ @item1 => 30 })
+      expect(@food_truck1.check_stock(@item1)).to eq(30)
+
+      @food_truck1.stock(@item1, 25)
+      expect(@food_truck1.check_stock(@item1)).to eq(55)
+
+      @food_truck1.stock(@item2, 12)
+      expect(@food_truck1.inventory).to eq({ @item1 => 55, @item2 => 12 })
+    end
+  end
+
+  describe "#revenue" do
+    before(:each) do
+      @food_truck1.stock(@item1, 35)
+      @food_truck1.stock(@item2, 7)
+      @food_truck2.stock(@item4, 50)
+      @food_truck2.stock(@item3, 25)
+      @food_truck3.stock(@item1, 65)
+    end
+
+    it "can list potential revenue" do
     end
   end
 end
