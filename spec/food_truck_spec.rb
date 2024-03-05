13ebe48cd056d7e28f054af4d2d67fa5c104306c
NeilTheSeal <neil.hendren@gmail.com>
2024-03-05 10:08:24 -0700

    Add test for potential revenue

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -52,6 +52,9 @@ RSpec.describe FoodTruck do
     end
 
     it "can list potential revenue" do
+      expect(@food_truck1.potential_revenue).to eq(148.75)
+      expect(@food_truck2.potential_revenue).to eq(345.00)
+      expect(@food_truck3.potential_revenue).to eq(243.75)
     end
   end
 end
