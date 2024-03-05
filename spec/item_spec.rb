2a65b74ca574a95868a9bdbce1e21c1985150feb
Jared Hobson <jaredmhobson@gmail.com>
2024-03-05 08:31:53 -0800

    Add #name and #price method tests in item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe Item do
     it 'exists' do
       expect(item1).to be_a(Item)
     end
+
+    it 'has a name' do
+      expect(item2.name).to eq('Apple Pie (Slice)')
+    end
+
+    it 'has a price' do
+      expect(item2.price).to eq(2.50)
+    end
   end
 end
