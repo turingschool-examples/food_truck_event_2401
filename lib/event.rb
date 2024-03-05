466305fabaa73a7b55c551291a6be96dd91313bd
Billy Wallace <wallace.billy1@gmail.com>
2024-03-05 09:44:49 -0700

    Create add_food_truck method in FoodTruck

--- lib/event.rb
+++ lib/event.rb
@@ -1,9 +1,12 @@
 class Event
   attr_reader :food_trucks
-  
+
   def initialize
     @food_trucks = []
-    
+  end
+
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck
   end
 
 end
