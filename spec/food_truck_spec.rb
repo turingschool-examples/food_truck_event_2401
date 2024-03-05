9ea6a751123616dfd1dddbbbcd728905b48e4441
Jared Hobson <jaredmhobson@gmail.com>
2024-03-05 08:41:36 -0800

    Add #check_stock tests in food_truck_spec

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,6 +4,8 @@ require './lib/event'
 
 RSpec.describe FoodTruck do
   let (:food_truck) { FoodTruck.new("Rocky Mountain Pies") }
+  let (:item1) { Item.new({name: 'Peach Pie (Slice)', price: "$3.75"}) }
+  let (:item2) { Item.new({name: 'Apple Pie (Slice)', price: '$2.50'}) }
 
   describe '#initialize' do
     it 'exists' do
@@ -16,6 +18,18 @@ RSpec.describe FoodTruck do
 
     it 'has no inventory by default' do
       expect(food_truck.inventory).to eq({})
+    end
+  end
+
+  describe '#check_stock' do
+    it 'returns 0 if there is no stock of an item' do
+      expect(food_truck.check_stock(item1)).to eq(0)
+    end
+
+    it 'can return the stocked amount of an item' do
+      food_truck.stock(item1, 30)
+
+      expect(food_truck.check_stock(item1)).to eq(30)
     end
   end
 end
