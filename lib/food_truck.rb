e1a9a8c562d5bee5e91883ec44473e68ce48db42
Billy Wallace <wallace.billy1@gmail.com>
2024-03-05 09:39:19 -0700

    Add inventory, stock, and check_stock methods in FoodTruck

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -2,6 +2,19 @@ class FoodTruck
   attr_reader :name
 
   def initialize (name)
+    @inventory = Hash.new(0)
     @name = name
+  end
+
+  def inventory
+    @inventory
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
+  end
+
+  def check_stock(item)
+    @inventory[item]
   end
 end
