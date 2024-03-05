56041a7f445f1a94afaa23069a9cbeb01fe48bb7
Billy Wallace <wallace.billy1@gmail.com>
2024-03-05 11:23:14 -0700

    Add total_inventory method to Event

--- lib/event.rb
+++ lib/event.rb
@@ -32,7 +32,22 @@ class Event
     all_items.uniq.sort
   end
 
-  # sorted_item_list => Array with a list of names (no dupes) of all items that foodtrucks have in stock, alpha order
-  # total_inventory => Hash with Items as keys and sub-hash as values.  Subhash should contain quantity and food_truck keys
+  def total_inventory
+    total_inventory = Hash.new
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, quantity| 
+        total_inventory[item] = {quantity: 0, food_trucks: []}
+      end
+    end
+
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, quantity|
+        total_inventory[item][:quantity] += quantity
+        total_inventory[item][:food_trucks] << food_truck
+      end
+    end
+    total_inventory
+  end
+
   #overstock_items => Array of Item objects.
 end
