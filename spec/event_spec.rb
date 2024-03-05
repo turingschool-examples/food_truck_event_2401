fb85710306c980cd8ccc5d2a9a21ececa89f4d72
Grant Davis <grantdavis303@gmail.com>
2024-03-05 11:09:15 -0700

    Formatting Fix

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -57,7 +57,6 @@ RSpec.describe Event do
         food_truck1.stock(item1, 30)
         food_truck2.stock(item1, 50)
         food_truck3.stock(item2, 48)
-
         event.add_food_truck(food_truck1)    
         event.add_food_truck(food_truck2)
         event.add_food_truck(food_truck3)
@@ -65,7 +64,7 @@ RSpec.describe Event do
         expect(event.food_trucks_that_sell(item1)).to eq [food_truck1, food_truck2]
     end
 
-    it "Event can return a sorted list of names of all items in food trucks" do
+    it "Event can return a sorted list of item names in all food trucks" do
         event = Event.new("South Pearl Street Farmers Market")
         food_truck1 = FoodTruck.new("Rocky Mountain Pies")
         food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
@@ -78,13 +77,10 @@ RSpec.describe Event do
         food_truck1.stock(item2, 50)
         food_truck2.stock(item3, 30)
         food_truck2.stock(item4, 50)
-
         event.add_food_truck(food_truck1)    
         event.add_food_truck(food_truck2)
 
         expect(event.sorted_item_list).to eq ["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"]
     end
-
-
 
 end
