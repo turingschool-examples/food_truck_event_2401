ecb2ce0de35edcf62808f58ae6200d81106e25d2
Grant Davis <grantdavis303@gmail.com>
2024-03-05 10:25:44 -0700

    Event can add food trucks

--- lib/event.rb
+++ lib/event.rb
@@ -1,8 +1,13 @@
 class Event
-    attr_reader :name
+    attr_reader :name, :food_trucks
 
     def initialize(name)
         @name = name
+        @food_trucks = Array.new
+    end
+
+    def add_food_truck(food_truck)
+        @food_trucks << food_truck
     end
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -19,4 +19,20 @@ RSpec.describe Event do
         expect(event.name).to eq "South Pearl Street Farmers Market"
     end
 
+    it "Event has an no food trucks by default" do
+        event = Event.new("South Pearl Street Farmers Market")
+        expect(event.food_trucks).to eq []
+    end
+
+    it "Event can add food trucks" do
+        event = Event.new("South Pearl Street Farmers Market")
+        food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+        food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+        
+        event.add_food_truck(food_truck1)    
+        event.add_food_truck(food_truck2)
+
+        expect(event.food_trucks).to eq [food_truck1, food_truck2]
+    end
+
 end
