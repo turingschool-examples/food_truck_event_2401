dd59b78ed8285895bd720f4eadd3485c75dcbaba
Grant Davis <grantdavis303@gmail.com>
2024-03-05 10:05:00 -0700

    Food Truck can check stock for specific item

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -24,4 +24,11 @@ RSpec.describe FoodTruck do
         expect(food_truck.inventory).to be_a Hash
     end
 
+    it "Food Truck can check stock for specific item" do
+        food_truck = FoodTruck.new("Rocky Mountain Pies")
+        item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+        expect(food_truck.check_stock(item1)).to eq 0
+    end
+
 end
