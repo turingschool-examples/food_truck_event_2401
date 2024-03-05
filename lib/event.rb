70dc6a97247453259fbd71fe9d8c2c852c9083f4
Jared Hobson <jaredmhobson@gmail.com>
2024-03-05 10:48:29 -0800

    Add #overstock_items method in event

--- lib/event.rb
+++ lib/event.rb
@@ -21,4 +21,34 @@ class Event
   def sorted_item_list
     @food_trucks.flat_map(&:list_of_item_names).uniq.sort
   end
+
+  def list_all_items
+    @food_trucks.flat_map(&:in_stock_items_list).uniq
+  end
+
+  # def total_inventory
+  #   inventory = Hash.new { |hash, key| hash[key] = {}}
+  #   @food_trucks.each do |truck|
+  #     truck.inventory.each do |item, amount|
+  #       inventory[item] = {
+  #         quantity: amount,
+  #         food_trucks: food_trucks_that_sell(item)
+  #       }
+  #     end
+  #   end
+  #   inventory
+  # end
+
+  def total_stock(item)
+    @food_trucks.sum do |truck|
+      truck.inventory[item]
+    end
+  end
+
+  def overstock_items
+    list_all_items.select do |item|
+      food_trucks_that_sell(item).count > 1 &&
+      total_stock(item) > 50
+    end
+  end
 end
