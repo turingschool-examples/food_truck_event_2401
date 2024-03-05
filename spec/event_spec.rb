cfaf23823a0138ea8c70027fec0b82122c498a00
Jared Hobson <jaredmhobson@gmail.com>
2024-03-05 09:39:45 -0800

    Add #sorted_item_list method in event_spec

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,14 +3,14 @@ require './lib/food_truck'
 require './lib/event'
 
 RSpec.describe Event do
-  let (:event) { Event.new("South Pearl Street Farmers Market") }
-  let (:food_truck1) { FoodTruck.new("Rocky Mountain Pies") }
-  let (:food_truck2) { FoodTruck.new("Ba-Nom-a-Nom") }
-  let (:food_truck3) { FoodTruck.new("Palisade Peach Shack") }
-  let (:item1) { Item.new({name: 'Peach Pie (Slice)', price: "$3.75"}) }
+  let (:event) { Event.new('South Pearl Street Farmers Market') }
+  let (:food_truck1) { FoodTruck.new('Rocky Mountain Pies') }
+  let (:food_truck2) { FoodTruck.new('Ba-Nom-a-Nom') }
+  let (:food_truck3) { FoodTruck.new('Palisade Peach Shack') }
+  let (:item1) { Item.new({name: 'Peach Pie (Slice)', price: '$3.75'}) }
   let (:item2) { Item.new({name: 'Apple Pie (Slice)', price: '$2.50'}) }
-  let (:item3) { Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"}) }
-  let (:item4) { Item.new({name: "Banana Nice Cream", price: "$4.25"}) }
+  let (:item3) { Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'}) }
+  let (:item4) { Item.new({name: 'Banana Nice Cream', price: '$4.25'}) }
 
   describe '#initialize' do
     it 'exists' do
@@ -42,11 +42,11 @@ RSpec.describe Event do
     it 'can list the names of all the food trucks' do
       event.add_food_truck(food_truck1)
 
-      expect(event.food_truck_names).to eq(["Rocky Mountain Pies"])
+      expect(event.food_truck_names).to eq(['Rocky Mountain Pies'])
 
       event.add_food_truck(food_truck2)
       
-      expect(event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom"])
+      expect(event.food_truck_names).to eq(['Rocky Mountain Pies', 'Ba-Nom-a-Nom'])
     end
   end
 
@@ -91,6 +91,29 @@ RSpec.describe Event do
       new_item1_trucks = event.food_trucks_that_sell(item1)
 
       expect(new_item1_trucks).to eq([food_truck1])
+    end
+  end
+
+  describe '#sorted_item_list' do
+    it 'can return a list of names of all items the food trucks have in stock sorted alphabetically' do
+      food_truck1.stock(item1, 35)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+  
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      items = ['Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream']
+
+      expect(event.sorted_item_list).to eq(items)
+
+      food_truck1.stock(item2, 7)
+
+      new_items = ['Apple Pie (Slice)', 'Banana Nice Cream', 'Peach Pie (Slice)', 'Peach-Raspberry Nice Cream']
+
+      expect(event.sorted_item_list).to eq(new_items)
     end
   end
 end
