07d750d3aaa5b399110bb62d9eff02889ac93a34
NeilTheSeal <neil.hendren@gmail.com>
2024-03-05 09:40:13 -0700

    Add tests for initialization

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe FoodTruck do
   before(:each) do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
   end
+
+  describe "#initialize" do
+    it "exists" do
+      expect(@food_truck).to be_an_instance_of(FoodTruck)
+    end
+
+    it "has attributes" do
+      expect(@food_truck.name).to eq("Rocky Mountain Pies")
+      expect(@food_truck.inventory).to eq({})
+    end
+  end
 end
