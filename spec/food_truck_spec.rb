5e37e2cc5dde259c7065e3cdf9394c38fdde33de
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-05 10:11:31 -0700

    Fix tabbing

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe FoodTruck do
   end
 
   describe "#initialize" do
-  it "exists" do
-    expect(@food_truck).to be_a(FoodTruck)
-  end
+    it "exists" do
+      expect(@food_truck).to be_a(FoodTruck)
+    end
 
-  it "has attributes" do
-    expect(@food_truck.name).to eq("Rocky Mountain Pies")
-    expect(@food_truck.inventory).to eq({})
+    it "has attributes" do
+      expect(@food_truck.name).to eq("Rocky Mountain Pies")
+      expect(@food_truck.inventory).to eq({})
+    end
   end
-end
 
 end
