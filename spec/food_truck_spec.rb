30cf5e14b16e3cff8e789758bb866711315b54a7
NeilTheSeal <neil.hendren@gmail.com>
2024-03-05 09:47:51 -0700

    Add more tests to item stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,5 +1,6 @@
 require "./spec/spec_helper"
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe FoodTruck do
   before(:each) do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
@@ -27,6 +28,11 @@ RSpec.describe FoodTruck do
       @food_truck.stock(@item1, 30)
       expect(@food_truck.inventory).to eq({ @item1 => 30 })
       expect(@food_truck.check_stock(@item1)).to eq(30)
+      @food_truck.stock(@item1, 25)
+      expect(@food_truck.check_stock(@item1)).to eq(55)
+      @food_truck.stock(@item2, 12)
+      expect(@food_truck.inventory).to eq({ @item1 => 55, @item2 => 12 })
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
