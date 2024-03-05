eae1061f55e11e93db57794ecd1675833704304a
Jared Hobson <jaredmhobson@gmail.com>
2024-03-05 08:52:47 -0800

    Add instantiations and exist test in event_spec

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -2,12 +2,19 @@ require './lib/item'
 require './lib/food_truck'
 require './lib/event'
 
-RSpec.describe Class do
-  let (:instance) { Class.new }
+RSpec.describe Event do
+  let (:event) { Event.new("South Pearl Street Farmers Market") }
+  let (:food_truck1) { FoodTruck.new("Rocky Mountain Pies") }
+  let (:food_truck2) { FoodTruck.new("Ba-Nom-a-Nom") }
+  let (:food_truck3) { FoodTruck.new("Palisade Peach Shack") }
+  let (:item1) { Item.new({name: 'Peach Pie (Slice)', price: "$3.75"}) }
+  let (:item2) { Item.new({name: 'Apple Pie (Slice)', price: '$2.50'}) }
+  let (:item3) { Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"}) }
+  let (:item4) { Item.new({name: "Banana Nice Cream", price: "$4.25"}) }
 
   describe '#initialize' do
     it 'exists' do
-      expect(instance).to be_a(Class)
+      expect(event).to be_a(Event)
     end
   end
 end
