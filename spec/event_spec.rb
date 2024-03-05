5d6e7588ca3bdd244f28c5e731f4a5928be76012
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-03-05 12:05:08 -0700

    Add sorted items list

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -68,20 +68,34 @@ RSpec.describe Event do
       expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
       expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
     end
-  end
+  
 
-  it 'displays the potential revenue of each' do 
-    @food_truck1.stock(@item1, 35)    
-    @food_truck1.stock(@item2, 7) 
-    @food_truck2.stock(@item4, 50)    
-    @food_truck2.stock(@item3, 25)
-    @food_truck3.stock(@item1, 65)
-    @event.add_food_truck(@food_truck1)    
-    @event.add_food_truck(@food_truck2)    
-    @event.add_food_truck(@food_truck3)
+    it 'displays the potential revenue of each' do 
+      @food_truck1.stock(@item1, 35)    
+      @food_truck1.stock(@item2, 7) 
+      @food_truck2.stock(@item4, 50)    
+      @food_truck2.stock(@item3, 25)
+      @food_truck3.stock(@item1, 65)
+      @event.add_food_truck(@food_truck1)    
+      @event.add_food_truck(@food_truck2)    
+      @event.add_food_truck(@food_truck3)
 
-    expect(@food_truck1.potential_revenue).to eq(148.75)
-    expect(@food_truck2.potential_revenue).to eq(345.00)
-    expect(@food_truck3.potential_revenue).to eq(243.75)
-  end
+      expect(@food_truck1.potential_revenue).to eq(148.75)
+      expect(@food_truck2.potential_revenue).to eq(345.00)
+      expect(@food_truck3.potential_revenue).to eq(243.75)
+    end
+
+    it 'sorts items alphebetically' do 
+      @food_truck1.stock(@item1, 35)    
+      @food_truck1.stock(@item2, 7) 
+      @food_truck2.stock(@item4, 50)    
+      @food_truck2.stock(@item3, 25)
+      @food_truck3.stock(@item1, 65)
+      @event.add_food_truck(@food_truck1)    
+      @event.add_food_truck(@food_truck2)    
+      @event.add_food_truck(@food_truck3)
+
+      expect(@event.sorted_item_list).to eq(["Apple Pie (Slice)", "Peach Pie (Slice)", "Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peach Pie (Slice)"])
+    end
+  end 
 end
