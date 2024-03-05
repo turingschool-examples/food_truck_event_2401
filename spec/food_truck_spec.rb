c60847d67e25f00b33618d8cb29bf88dd2386aed
Jared Hobson <jaredmhobson@gmail.com>
2024-03-05 08:36:02 -0800

    Add #initialize method test and attributes in food_truck_spec

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,12 +2,20 @@ require './lib/item'
 require './lib/food_truck'
 require './lib/event'
 
-RSpec.describe Class do
-  let (:instance) { Class.new }
+RSpec.describe FoodTruck do
+  let (:food_truck) { FoodTruck.new("Rocky Mountain Pies") }
 
   describe '#initialize' do
     it 'exists' do
-      expect(instance).to be_a(Class)
+      expect(food_truck).to be_a(FoodTruck)
+    end
+
+    it 'has a name' do
+      expect(food_truck.name).to eq('Rocky Mountain Pies')
+    end
+
+    it 'has no inventory by default' do
+      expect(food_truck.inventory).to eq({})
     end
   end
 end
