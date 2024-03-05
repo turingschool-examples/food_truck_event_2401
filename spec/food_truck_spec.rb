88148b90093d1434daa9c31044c221ecdfdaff81
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-03-05 10:24:58 -0700

    Add more stock to item 2 and put item 1 and 2 in before each block as well

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,6 +4,8 @@ require './lib/food_truck'
 RSpec.describe FoodTruck do
   before(:each) do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   describe '#initialize' do
@@ -21,7 +23,9 @@ RSpec.describe FoodTruck do
     expect(@food_truck.check_stock(@item1)).to eq(30)
     @food_truck.stock(@item1, 25)
     expect(@food_truck.check_stock(@item1)).to eq(55)
-
+    @food_truck.stock(@item2, 12)
+    expect(@food_truck.check_stock(@item2)).to eq(12)
+    expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
     end
   end
 end
