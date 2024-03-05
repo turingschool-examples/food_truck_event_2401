5ee22de5404738dff41d6f5e299409acc9237648
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-03-05 09:39:03 -0700

    Add before each and initialize block

---
+++ spec/item_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@item2.name).to eq("Apple Pie (Slice)")
+      expect(@item2.price).to eq(2.50)
+    end
+  end
+end
