b7fa57e6277ceb1287ebc04959f6105ceb6131c4
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-03-05 10:42:59 -0700

    Add inventory to food truck 3

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Event do
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack") 
   end
 
   describe '#initialize' do
@@ -33,6 +34,12 @@ RSpec.describe Event do
       @food_truck2.stock(@item3, 25)
 
       expect(@food_truck2.inventory).to eq({@item4 => 50, @item3 => 25})
+    end
+
+    it 'displays what is in food truck 3' do 
+      @food_truck3.stock(@item1, 65)
+
+      expect(@food_truck3.inventory).to eq({@item1 => 65})
     end
   end
 end
