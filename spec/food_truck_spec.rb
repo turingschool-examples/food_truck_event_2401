9b94962a041cf5193d0fdf72255d43da8d5f0c97
NeilTheSeal <neil.hendren@gmail.com>
2024-03-05 09:42:26 -0700

    Add test for checking stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,6 +3,8 @@ require "./spec/spec_helper"
 RSpec.describe FoodTruck do
   before(:each) do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({ name: "Peach Pie (Slice)", price: "$3.75" })
+    @item2 = Item.new({ name: "Apple Pie (Slice)", price: "$2.50" })
   end
 
   describe "#initialize" do
@@ -13,6 +15,12 @@ RSpec.describe FoodTruck do
     it "has attributes" do
       expect(@food_truck.name).to eq("Rocky Mountain Pies")
       expect(@food_truck.inventory).to eq({})
+    end
+  end
+
+  describe "#stock" do
+    it "can check stock of an item" do
+      expect(@food_truck.check_stock(@item1)).to eq(0)
     end
   end
 end
