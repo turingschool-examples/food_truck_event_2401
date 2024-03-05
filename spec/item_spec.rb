8ece7910c1701ef1c046e98e2f0b56fd9f07c658
Billy Wallace <wallace.billy1@gmail.com>
2024-03-05 09:28:32 -0700

    Add test for initializing Item objects

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,5 +4,12 @@ require './lib/item'
 require './lib/food_truck'
 
 describe Item do
- 
+  before :each do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  it 'exists' do
+    expect(@item).to be_an Item
+  end
 end
