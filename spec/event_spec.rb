e579256fcb37d8394cb6407ffcab5bdf7a86cf97
NeilTheSeal <neil.hendren@gmail.com>
2024-03-05 09:55:57 -0700

    Add test for adding food trucks

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,8 +1,28 @@
 require "./spec/spec_helper"
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe Event do
   before(:each) do
     @event = Event.new("South Pearl Street Farmers Market")
+
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+    @item1 = Item.new({ name: "Peach Pie (Slice)", price: "$3.75" })
+    @item2 = Item.new({ name: "Apple Pie (Slice)", price: "$2.50" })
+    @item3 = Item.new({ name: "Peach-Raspberry Nice Cream", price: "$5.30" })
+    @item4 = Item.new({ name: "Banana Nice Cream", price: "$4.25" })
+
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(item4, 50)
+    @food_truck2.stock(item3, 25)
+    @food_truck3.stock(item1, 65)
+
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
   end
 
   describe "#initialize" do
@@ -15,4 +35,13 @@ RSpec.describe Event do
       expect(@event.food_trucks).to eq([])
     end
   end
+
+  describe "#food trucks" do
+    it "can add food trucks" do
+      expect(@event.food_trucks).to eq([
+        @food_truck1, @food_truck2, @food_truck3
+      ])
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
