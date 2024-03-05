e8a54d87f2d8c90a9972fe7b36a1fc49643e2fa5
Jared Hobson <jaredmhobson@gmail.com>
2024-03-05 09:12:07 -0800

    Add #food_trucks_that_sell method in event

--- lib/event.rb
+++ lib/event.rb
@@ -9,4 +9,12 @@ class Event
   def add_food_truck(truck)
     @food_trucks << truck
   end
+
+  def food_truck_names
+    @food_trucks.map(&:name)
+  end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.select { |truck| truck.check_stock(item) > 0 }
+  end
 end
