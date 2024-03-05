b7971ff82f318805f09674ca1d1c10022ee365c2
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-03-05 11:28:13 -0700

    Add potential revenue tests for each truck

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -52,10 +52,25 @@ RSpec.describe Event do
     expect(@event.food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
     expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
-end
+  end
 
-describe '#food trucks that sell' do
-  it 'displays what food trucks sell' do
+  describe '#food trucks that sell' do
+    it 'displays what food trucks sell' do
+      @food_truck1.stock(@item1, 35)    
+      @food_truck1.stock(@item2, 7) 
+      @food_truck2.stock(@item4, 50)    
+      @food_truck2.stock(@item3, 25)
+      @food_truck3.stock(@item1, 65)
+      @event.add_food_truck(@food_truck1)    
+      @event.add_food_truck(@food_truck2)    
+      @event.add_food_truck(@food_truck3)
+      
+      expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
+      expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
+    end
+  end
+
+  it 'displays the potential revenue of each' do 
     @food_truck1.stock(@item1, 35)    
     @food_truck1.stock(@item2, 7) 
     @food_truck2.stock(@item4, 50)    
@@ -64,10 +79,9 @@ describe '#food trucks that sell' do
     @event.add_food_truck(@food_truck1)    
     @event.add_food_truck(@food_truck2)    
     @event.add_food_truck(@food_truck3)
-      
-    expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
-    expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
 
-    end
+    expect(@food_truck1.potential_revenue).to eq(148.75)
+    expect(@food_truck2.potential_revenue).to eq(345.00)
+    expect(@food_truck3.potential_revenue).to eq(243.75)
   end
 end
