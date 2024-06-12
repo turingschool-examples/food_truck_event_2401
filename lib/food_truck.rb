983b4ce1e9f051a6debf30456e1ff16abb28a541
Joel Davalos <jsdavalos@gmail.com>
2024-06-12 10:02:27 -0600

    Inital commit

---
+++ lib/food_truck.rb
@@ -0,0 +1,24 @@
+class FoodTruck
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    if @inventory[item]
+      @inventory[item]
+    else
+      0
+    end
+  end
+
+  def stock(item, quantity)
+    if @inventory.key?(item)
+      @inventory[item] += quantity 
+    else 
+      @inventory[item] = quantity
+    end
+  end
+end
